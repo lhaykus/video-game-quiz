48b6bfd6611562fdf9f1aa4eaec388ab06eedc5f
Loryn <lorynhaykus@yahoo.com>
2021-05-10 21:58:53 -0600

    linking all other pages and creating the questions

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+//Create an array with objects of the questions, answers and possible answers
+const questions = [ {
+    question: "Who created the T-virus in Resident Evil?",
+    rightAnswer: "The Umbrella Corporation",
+    wrongAnswers:
+    "The Raccoon Corporation",
+    "Dynamite Corporation",
+    ""
+    
+}
+]
