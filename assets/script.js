ac6e5af02ca7f641ad41964c2f7d0c835ec87a86
Loryn <lorynhaykus@yahoo.com>
2021-05-11 02:17:33 -0600

    starting code

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,26 @@
 //Create an array with objects of the questions, answers and possible answers
 const questions = [ {
     question: "Who created the T-virus in Resident Evil?",
-    rightAnswer: "The Umbrella Corporation",
-    wrongAnswers:
-    "The Raccoon Corporation",
-    "Dynamite Corporation",
-    ""
+    choices: ["The Raccoon Corporation", "The Umbrella Corporation", "Dynamite Corporation", "Boomerang Corporation"],
+    answer: "The Umbrella Corporation"
     
+}, { 
+    question: "What is the alien race that has to battle Predator in Alien vs Predator called?",
+    choices: ["Oliomorph", "Polymorph","Xenomorph", "Demimorph"],
+    answer: "Xenomorph"
+}, 
+{
+    question: ""
 }
 ]
+
+
+
+
+
+//Declare variables
+
+//Set up buttons
+
+//Set up timer
+
