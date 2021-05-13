cfc812166613b89f467cb1ffbed4fc2ff5c7ec07
Loryn <lorynhaykus@yahoo.com>
2021-05-13 14:53:10 -0600

    got questions to show up

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,14 @@
+//Grabbing elements from the HTML document
+const timer = document.getElementById("timer");
+const startButton = document.getElementById("startButton");
+const quiz = document.getElementById("#quiz-container");
+const questionEl = document.getElementById("question");
+const answerBtn = document.getElementById("answerBtn");
+const buttons = document.getElementsByClassName("buttons");
+const questionSection = document.getElementById("question-section");
+
+
+
 //Create an array with objects of the questions, answers and possible answers
 const questions = [ {
     question: "Who created the T-virus in Resident Evil?",
@@ -26,69 +37,74 @@ const questions = [ {
 },
 ];
 
-//Grabbing elements from the HTML document
-const timer = document.getElementById("timer");
-const startButton = document.getElementById(".startbtn");
-const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
-const button1 = document.getElementById("button1");
-const button2 = document.getElementById("button2");
-const button3 = document.getElementById("button3");
-const button4 = document.getElementById("button4");
 
 //Declaring variables
 const lastQuestion = question.length- 1;
-const currentQuestion =0;
+let currentQuestion =0;
 const correctAnswers=0;
-const question = "none";
+//let question = "none";
 const answer = "none"; 
 const totalTime = 90;
-const secondsLeft = 0;
-const choiceButtons; 
+let secondsLeft = 0;
+let choiceButtons; 
 
+
+startButton.addEventListener("click", startQuiz)
 
 //Create a start function to start the game
-const startQuiz = function() {
+function startQuiz() {
+    console.log("start");
+    startButton.classList.add('hide');
+    currentQuestion = 0;
+    questionSection.classList.remove('hide');
+    nextQuestion();
+
 
 }
 
 
-//Function to show question on the screen 
 
-function renderQuestion() {
-    question.textContnet
-    //const q = question.[currentQuestion];
-   // question.innerHTML = "" + question[currentQuestion].question;
+
+//Function to go to the next question
+function nextQuestion() {
+    showQuestion(questions[currentQuestion]);
+}
+
+//Function to show question on screen 
+function showQuestion(question) {
+    questionEl.innerText = question.question;
 
 }
+
+
 
 
 //Function to check the answer with the users choice
-function checkAnswer(answer){
+//function checkAnswer(answer){
     //If users choice matches the right answer, then correctAnswers score increaes
-    if(question[currentQuestion].correct===answer) {
-        correctAnswers++;
-        checkAnswer.textContnet = "Correct!"
+ //   if(question[currentQuestion].correct===answer) {
+ //       correctAnswers++;
+  //      checkAnswer.textContnet = "Correct!"
     
     //Else if the users choice does NOT match the right answer, 10 seconds is taken off the timer
     //and a message
-    } else {
-        totalTime -=10;
-        secondsLeft.textContnet=totalTime;
-        checkAnswer.textContnet = "Wrong."
+//    } else {
+//        totalTime -=10;
+ //       secondsLeft.textContnet=totalTime;
+    //    checkAnswer.textContnet = "Wrong."
 
-    }
+   // }
     //If the current question is not the last question, then the next question will be displayed
-    if(currentQuestion < lastQuestion) {
-        currentQuestion++;
-        renderQuestion();
-    }
+  //  if(currentQuestion < lastQuestion) {
+  //      currentQuestion++;
+   //     renderQuestion();
+   // }
     //If the current question is the last question, the timer will be cleared and the 
-    if(currentQuestion >= lastQuestion) {
-        clearInterval(timer);
+   // if(currentQuestion >= lastQuestion) {
+  //      clearInterval(timer);
         
-    }
-}
+   // }
+//}
 
 
 
@@ -102,7 +118,7 @@ function checkAnswer(answer){
 
 //Set up timer
 //Get timer to start when start game is pressed
-function setTimer() {
+/*function setTimer() {
 const timer = setInterval(function(){
     secondsLeft--;
     timer.textContnet = "Timer: " + secondsLeft;
@@ -114,10 +130,7 @@ const timer = setInterval(function(){
 }, 9000);
 }
 
-//Show the questions and choices
-function quiz() {
-    nextQuestoin();
-}
+
 
 
 
@@ -130,9 +143,7 @@ function quiz() {
 
 
 //Event listener to call startGame function when button is clicked
-startButton.addEventListener("click", startGame);
 
-button1.addEventListener("click", checkAnswer);
-button2.addEventListener("click", checkAnswer);
-button3.addEventListener("click", checkAnswer);
-button4.addEventListener("click", checkAnswer);
+
+
+*/
