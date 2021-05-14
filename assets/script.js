07700f2fa06b270c25c8c1c44892b1b485f1df1c
Loryn <lorynhaykus@yahoo.com>
2021-05-13 18:50:31 -0600

    got the questions to change to the next when a choice is clicked

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,58 @@
 //Grabbing elements from the HTML document
 const timer = document.getElementById("timer");
 const startButton = document.getElementById("startButton");
-const quiz = document.getElementById("#quiz-container");
+const quiz = document.getElementById("quiz-container");
 const questionEl = document.getElementById("question");
-const answerBtn = document.getElementById("answerBtn");
+const choiceBtn = document.getElementById("choiceBtn");
 const buttons = document.getElementsByClassName("buttons");
 const questionSection = document.getElementById("question-section");
+const choiceSection = document.getElementsByClassName("#choices");
+const choiceA = document.getElementById ("A");
+const choiceB = document.getElementById ("B");
+const choiceC = document.getElementById ("C");
+const choiceD = document.getElementById ("D");
 
 
 
 //Create an array with objects of the questions, answers and possible answers
 const questions = [ {
     question: "Who created the T-virus in Resident Evil?",
-    choices: ["The Raccoon Corporation", "The Umbrella Corporation", "Dynamite Corporation", "Boomerang Corporation"],
+    choiceA: "The Racoon Corporation",
+    choiceB: "The Umbrella Corporation",
+    choiceC: "Dynamite Corporation",
+    choiceD: "Boomerang Corporation",
     answer: "The Umbrella Corporation"
     
 }, { 
     question: "What is the alien race that has to battle Predator in Alien vs Predator called?",
-    choices: ["Kaminoan", "Polymorph","Xenomorph", "Ortolan"],
+    choiceA: "Kaminoan",
+    choiceB: "Polymorph", 
+    choiceC: "Xenomorph", 
+    choiceD: "Ortolan",
     answer: "Xenomorph"
 }, 
 {
     question: "In Harry Potter, what was the name of Hagrid's hippogriff?",
-    choices: ["Buckbeak", "Fawkes", "Aragog", "Hedwig"],
+    choiceA: "Buckbeak",
+    choiceB: "Fawkes", 
+    choiceC: "Aragog", 
+    choiceD: "Hedwig",
     answer: "Buckbeak"
 }, 
 {
     question: "In Lord of the Rings, who was NOT a part of the fellowship of the ring?",
-    choices: ["Aragorn", "Gimli", "Frodo", "Eowyn"],
+    choiceA: "Aragorn", 
+    choiceB: "Gimli",
+    choiceC: "Frodo",
+    choiceD: "Eowyn",
     answer: "Eowyn"
 }, 
 {
     question: "In Futurama, what is Fry's favorite soft drink that is highly addictive?",
-    choices: ["Nuka-Cola", "Slurm", "Buzz Cola", "Butterbeer"],
+    choiceA: "Nuka-Cola",
+    choiceB: "Slurm",
+    choiceC: "Buzz Cola",
+    choiceD: "Butterbeer",
     answer: "Slurm"
 },
 ];
@@ -40,15 +60,19 @@ const questions = [ {
 
 //Declaring variables
 const lastQuestion = question.length- 1;
-let currentQuestion =0;
+let currentQuestion;
 const correctAnswers=0;
 //let question = "none";
+let choices = " ";
 const answer = "none"; 
 const totalTime = 90;
 let secondsLeft = 0;
 let choiceButtons; 
+let button;
+let choice;
 
 
+//When the start button is clicked, the quiz begins
 startButton.addEventListener("click", startQuiz)
 
 //Create a start function to start the game
@@ -58,52 +82,81 @@ function startQuiz() {
     currentQuestion = 0;
     questionSection.classList.remove('hide');
     nextQuestion();
-
-
 }
 
 
 
 
 //Function to go to the next question
+//Grabbing the current question out of the questions array
 function nextQuestion() {
     showQuestion(questions[currentQuestion]);
 }
 
 //Function to show question on screen 
-function showQuestion(question) {
-    questionEl.innerText = question.question;
 
+function showQuestion(question) {
+    //Displaying the question by changing the text of questionEl by grabbing the question text out of the object 
+    questionEl.textContent = question.question;
+   // changing the text for each choice by grabbing the current question out of the questions array and assigning the choiceA-D in the current question
+    choiceA.textContent = questions[currentQuestion].choiceA;
+    choiceB.textContent = questions[currentQuestion].choiceB;
+    choiceC.textContent = questions[currentQuestion].choiceC;
+    choiceD.textContent = questions[currentQuestion].choiceD;
+  
+}
+
+function selectAnswer () {
+currentQuestion ++;
+    nextQuestion();
+    console.log(selectAnswer);
 }
 
 
+  //  var q = questions[currentQuestion];
+   // choiceA.innerText = q.choiceA;
 
+ //   choiceA.textContent = choiceA[currentQuestion].choiceA;
+    // questions.choices.forEach(choices => {
+   //   questionEl.choices = document.createElement ('button');
+   //   button.innerText = choices.text;
+     //  button.classList.add("btn");
+
+   
+        
+    
+
+
+
+
+/*
 
 //Function to check the answer with the users choice
-//function checkAnswer(answer){
-    //If users choice matches the right answer, then correctAnswers score increaes
- //   if(question[currentQuestion].correct===answer) {
- //       correctAnswers++;
-  //      checkAnswer.textContnet = "Correct!"
+function selectAnswer(answer){
+ //   If users choice matches the right answer, then correctAnswers score increaes
+   if(choice === questions[currentQuestion].answer) {
+       currentQuestion++;
+      correctAnswers++;
+    checkAnswer.textContnet = "Correct!"
     
-    //Else if the users choice does NOT match the right answer, 10 seconds is taken off the timer
-    //and a message
-//    } else {
-//        totalTime -=10;
- //       secondsLeft.textContnet=totalTime;
-    //    checkAnswer.textContnet = "Wrong."
+   // Else if the users choice does NOT match the right answer, 10 seconds is taken off the timer
+   // and a message
+    } else {
+       totalTime -=10;
+    secondsLeft.textContnet=totalTime;
+       checkAnswer.textContnet = "Wrong."
 
-   // }
+    }
     //If the current question is not the last question, then the next question will be displayed
-  //  if(currentQuestion < lastQuestion) {
-  //      currentQuestion++;
-   //     renderQuestion();
-   // }
+   if(currentQuestion < lastQuestion) {
+       currentQuestion++;
+        nextQuestion();
+    }
     //If the current question is the last question, the timer will be cleared and the 
-   // if(currentQuestion >= lastQuestion) {
-  //      clearInterval(timer);
+   if(currentQuestion >= lastQuestion) {
+        clearInterval(timer);
         
-   // }
+    };
 //}
 
 
@@ -118,7 +171,7 @@ function showQuestion(question) {
 
 //Set up timer
 //Get timer to start when start game is pressed
-/*function setTimer() {
+function setTimer() {
 const timer = setInterval(function(){
     secondsLeft--;
     timer.textContnet = "Timer: " + secondsLeft;
@@ -128,22 +181,24 @@ const timer = setInterval(function(){
         alert("Time's Up");
     }
 }, 9000);
+
 }
+}
+ //let q = questions[currentQuestion];
+  //  
+  //  question.choices.forEach(choices => document.createElement('button'),button.textContent = q.choices);
+    
+      //  const button = document.createElement('button');
+      //  button.innerHTML = q.choices.value;
 
 
-
-
-
-
-
-
-
-
-
-
-
-//Event listener to call startGame function when button is clicked
-
-
+        //currentQuestion.choices.forEach(function(choices, i) {
+  //   const button = document.createElement('button');
+  //    button.setAttribute("value", choices);
+  //    button.classList.add('buttons');
+  //  button.textContent= i + 1 + ". " + choices;
+   // button.onclick = checkAnswer;
+   // choiceEl.appendChild(button);
+  //  });
 
 */
